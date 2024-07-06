752de48d8c626b4ff99ea6f3801ae117bf624f93
Martin Reid <66138946+lmr97@users.noreply.github.com>
2024-07-05 19:22:37 -0600

    initial commit

---
+++ LetterboxdFilm.py
@@ -0,0 +1,219 @@
+from requests import Session
+from selectolax.parser import HTMLParser
+
+TABBED_ATTRS = ["actor",
+                "additional-directing",
+                "additional-photography",
+                "art-direction",
+                "assistant-director",
+                "camera-operator",
+                "choreography",
+                "cinematography",
+                "composer",
+                "costume-design",
+                "country",
+                "director",
+                "editor",
+                "executive-producer",
+                "genre",
+                "hairstyling",
+                "language",
+                "lighting",
+                "makeup",
+                "mini-theme",
+                "original-writer",
+                "producer",
+                "production-design",
+                "set-decoration",
+                "songs",
+                "sound",
+                "special-effects",
+                "special-effects",
+                "story",
+                "studio",
+                "stunts",
+                "theme",
+                "title-design",
+                "visual-effects",
+                "writer"
+                ]
+
+class LetterboxdFilm:
+    """
+    This class gets the HTML for the pages relevant to a film on Letterboxd,
+    providing a simple interface for the film page's HTML.
+
+    The following information is retreived and stored as attributes upon 
+    initialization (all as `str` variables):
+    - Title
+    - Year
+    - Film page URL
+    - Film page HTML
+
+    Any other film information is accessed through CSS-based searches on the HTML, implemented
+    as class methods.
+    """
+    def __init__(self, film_url: str):
+        
+        insert_index = film_url.find("/film")
+        stats_url = film_url[:insert_index] + "/csi" + film_url[insert_index:] + "stats/"
+        self._stats_url = stats_url
+
+        with Session() as s:
+            page_response = s.get(film_url)
+
+            try: assert(page_response.status_code == 200)
+            except AssertionError:
+                print(f"\nInvalid URL: {film_url}\n")
+                exit()  
+
+            self._url = film_url
+            page_html = HTMLParser(page_response.text)
+
+            stats_response = s.get(stats_url)
+            stats_html = HTMLParser(stats_response.text)
+
+        self._html  = page_html
+        self._title = page_html.css("span.js-widont")[0].text()
+        self._year  = page_html.css("a[href^='/films/year/']")[0].text()
+        self._stats_html = stats_html
+
+
+    @property
+    def url(self):
+        return self._url
+    
+    @property
+    def title(self):
+        return self._title
+    
+    @property
+    def year(self):
+        return self._year
+    
+    @property
+    def page_html(self):
+        return self._html.html
+
+
+    def get_tabbed_attribute(self, attribute: str) -> list:
+        """
+        Returns data from the tabbed section of a Letterboxd film page 
+        where the cast, crew, details, genres, and releases info is  
+        (except for the Releases tab, as this section follows a different 
+        structure).
+
+        Will return `["Not listed"]` (a list with that string as its 
+        only element) if the attribute was not found for the given film, 
+        whether it was a valid attribute or not. An invalid argument warning is 
+        printed after a ValueError is raised if the attribute is not valid.
+        
+        Always use the full, singular form of the attribute you'd like, and replace each space
+        with a `-` (ASCII 45). 
+
+        See example below:
+
+        ```
+        >>> film = LetterboxdFilm("https://letterboxd.com/film/rango")
+        >>> film.get_tabbed_attribute("assistant-director")
+        >>> ['Adam Somner', 'Ian Calip']
+        ```
+        """
+
+        try: 
+            if (attribute not in TABBED_ATTRS):
+                raise ValueError
+        except (ValueError):
+            print("\nINVALID ARGUMENT ", "\"", attribute, "\" passed to LetterboxdFilm.get_tabbed_attribute()\n",
+                  "\nEXPLANATION: This film information is either not in the HTML by that name,\n",
+                  "     or there is no method imlemented to retireve it yet. \n",
+                  "SUGGESTED ACTION: If it seems like the latter is the case, \n",
+                  "     please open up a GitHub issue and I will work on \n",
+                  "     implementing a method to retrieve the information. \n",
+                  "     You can also submit a pull request and implement it yourself.\n",
+                  sep="")
+            pass
+            
+        elements = self._html.css("a[href*='/" + attribute + "/']")
+
+        # extract text from found HTML elements
+        attribute_list = [e.text() for e in elements]
+        
+        # return only distinct values, but still as a list
+        if (attribute_list): return list(set(attribute_list))
+
+        # the outcome whether the attribute was not valid or valid but not found for the film
+        else: return ["Not listed"]         
+    
+    
+    def get_directors(self) -> list:
+        """
+        For many films, there are multiple directors (e.g. The Matrix (1999)), 
+        so this method always returns a list.
+        """
+        return self.get_tabbed_attribute("director")
+    
+    def get_genres(self) -> list:
+        return self.get_tabbed_attribute("genre")
+    
+    def get_countries(self) -> list:
+        return self.get_tabbed_attribute("country")
+    
+    def get_studios(self) -> list:
+        return self.get_tabbed_attribute("studio")
+    
+    def get_actors(self) -> list:
+        return self.get_tabbed_attribute("actor")
+    
+    def get_themes(self) -> list:
+        return self.get_tabbed_attribute("theme")
+
+    def get_avg_rating(self) -> float:
+        rating_element = self._html.css("meta[name='twitter:data2']")
+    
+        avg_rating = None
+        if (len(rating_element) > 0):
+            rating_element_content = rating_element[0].attributes['content']
+            rating_element_title_parsed = rating_element_content.split(" ")
+            avg_rating = float(rating_element_title_parsed[0])
+
+        return avg_rating
+
+
+    def get_casting(self) -> dict:
+        """
+        Returns a `dict` with the actor names as keys, 
+        and character names as values.
+        """
+        actor_nodes = self._html.css("a[href*='/actor/']")
+
+        casting = {}
+        for node in actor_nodes:
+
+            # sometimes, people link actors in reviews; gotta filter those out
+            try: node.attrs['title']
+            except (KeyError): continue
+
+            casting[node.text()] = node.attrs['title'] 
+
+        return casting
+    
+    def get_watches(self) -> int:
+        watches_msg = self._stats_html.css("a.icon-watched")[0].attrs['title']
+        watches_msg = watches_msg[11:]                       # take out the "Watched by"
+        watches_msg = watches_msg[:-8]                       # take out the " members"
+        view_count  = watches_msg.replace(",", "")           # take out commas
+
+        return view_count
+    
+    def get_likes(self) -> int:
+        likes_msg = self._stats_html.css("a.icon-liked")[0].attrs['title']
+        likes_msg = likes_msg[9:]                          # take out the "Liked by"
+        likes_msg = likes_msg[:-8]                         # take out the " members"
+        likes_count = likes_msg.replace(",", "")           # take out commas
+
+        return likes_count
+
+
+
+        
---
+++ get_list.py
@@ -0,0 +1,228 @@
+# This gets the elements of a list on Letterboxd
+
+from sys import argv
+from shutil import get_terminal_size
+from math import ceil
+from argparse import ArgumentParser
+from fnmatch import filter
+from requests import Session
+from LetterboxdFilm import LetterboxdFilm
+from selectolax.parser import HTMLParser
+    
+
+# Superset of TABBED_ATTRS list in letterboxdFilm.py
+VALID_ATTRS  = ["actor",
+                "additional-directing",
+                "additional-photography",
+                "art-direction",
+                "assistant-director",
+                "avg_rating",
+                "camera-operator",
+                "casting",
+                "choreography",
+                "cinematography",
+                "composer",
+                "costume-design",
+                "country",
+                "director",
+                "editor",
+                "executive-producer",
+                "genre",
+                "hairstyling",
+                "language",
+                "lighting",
+                "likes",
+                "makeup",
+                "mini-theme",
+                "original-writer",
+                "producer",
+                "production-design",
+                "set-decoration",
+                "songs",
+                "sound",
+                "special-effects",
+                "special-effects",
+                "story",
+                "studio",
+                "stunts",
+                "theme",
+                "title-design",
+                "visual-effects",
+                "watches",
+                "writer"
+                ]
+
+def print_loading_bar(rows_now, total_rows):
+    output_width = get_terminal_size(fallback=(80,25))[0]-11    # runs every call to adjust as terminal changes
+
+    bar_width_now = ceil(output_width * (rows_now)/total_rows)
+
+    print("| ", "█" * bar_width_now, 
+            (output_width - bar_width_now) * " ", "|", 
+            f"{(rows_now)/total_rows:.0%}",
+            end = "\r")
+
+
+
+def get_list_with_attrs(letterboxd_list_url: str,
+                        attrs: list,
+                        output_file: str):
+    
+    print() # to give the loading bar some room to breathe
+    
+    list_file = []
+    unretrieved_attrs = []
+    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
+
+    with Session() as s:
+        s.headers['User-Agent'] = user_agent
+
+        with open(output_file, "w") as lbfile_writer:
+            
+            num_pages = 1  # update this value later
+            current_page = 1
+            listwide_vars_updated = False
+            list_is_ranked = False
+            list_rank = 1
+            while(True):
+                
+                listpage = s.get(letterboxd_list_url+"page/"+str(current_page)+"/")
+                tree = HTMLParser(listpage.text)
+                film_urls = ["https://letterboxd.com" + el.attrs['data-target-link']  for el in tree.css("div[data-target-link^='/film/']")]
+
+                # placed in if statement so it the CSS searches don't run every iteration
+                if (not listwide_vars_updated):
+                    page_num_nodes = tree.css("li.paginate-page")
+                    list_num_nodes = tree.css("p.list-number")
+                    
+                    if (page_num_nodes): 
+                        num_pages = int(page_num_nodes[-1].text())
+                     
+                    if (list_num_nodes):
+                        list_is_ranked = True
+                    
+                    listwide_vars_updated = True
+
+
+                for url in film_urls:
+  
+                    film = LetterboxdFilm(url)
+
+                    title = film.title.replace(",", "")   # to not mess up the CSV file
+                    file_row = title+","+film.year
+
+
+                    for attr in attrs:
+
+                        # look through dictionary of method names for attr...
+                        matches = filter(vars(LetterboxdFilm).keys(), "*"+attr)
+                        
+                        # ...and call the method associated with the given attr
+                        if (matches): 
+                            found_attr = vars(LetterboxdFilm)[matches[0]](film)
+                        
+
+                        # if it's not there, it could be a tabbed attribute, so try that
+                        else:
+                            try:
+                                found_attr = film.get_tabbed_attribute(attr)
+                            
+                            # keep track of unfound attrs, for header construction
+                            except (ValueError):
+                                unretrieved_attrs.append(attr)
+                                pass
+                        
+                        # convert list or dict to CSV-friendly format
+                        if (   type(found_attr) == list
+                            or type(found_attr) == dict):
+                            
+                            found_attr = str(found_attr)
+                            found_attr = found_attr[2:-2]               # take off brackets
+                            found_attr = found_attr.replace("\'", "")   # take out quote marks
+                            found_attr = found_attr.replace(",", ";")   # separate list/dict elements by ";", not ","
+
+                            file_row  += "," + found_attr
+                        else:
+                            file_row  += "," + str(found_attr)
+
+                    if (list_is_ranked): 
+                        file_row = str(list_rank) + "," + file_row
+                        list_rank += 1
+
+                    file_row += "\n"
+                    list_file.append(file_row)
+
+                    # make sure loading bar has the correct denominator
+                    if (len(film_urls) < 100):
+                        print_loading_bar(len(list_file), 100*num_pages - (100-len(film_urls)))
+                    else:
+                        print_loading_bar(len(list_file), len(film_urls)*num_pages)
+
+
+                if (current_page == num_pages): break
+                current_page += 1
+            
+
+            # finalize header
+            header = "Title,Year"
+            for attr in attrs:
+                if (attr not in unretrieved_attrs):
+                    header += "," + attr.capitalize()
+            
+            if (list_is_ranked): header = "Rank," + header
+
+            header += "\n"
+            list_file.insert(0, header)
+            lbfile_writer.writelines(list_file)
+
+
+# so the argparser will play nice with -h
+def defaultOutputFile():
+    if (len(argv) > 2): 
+        return argv[2].split("/")[-2]+".csv"   # use the list name in URL .csv
+    else: 
+        return ""
+
+
+def parse_CLI_args():
+    arg_parser = ArgumentParser(description="Collects requested film attributes from a given Letterboxd list, and puts it in a CSV file (title and year are automatically included, and rank if list is ranked)")
+
+    arg_parser.add_argument('-l','--list-url', 
+                            nargs=1, 
+                            type=str, 
+                            required=True, 
+                            help="The URL of the Letterboxd list."
+                            )
+    
+    arg_parser.add_argument('-a','--attributes', 
+                            nargs='*', 
+                            choices=VALID_ATTRS, 
+                            default=[],
+                            required=False, 
+                            help="The information about the film you'd like to add to the CSV. You can add as many attributes as you like from the list above, separated by spaces, or none at all. If none are given, only titles and years (and list rank, if applicable) are collected for the films."
+                            )
+    
+    arg_parser.add_argument('-o', '--output-file',
+                            nargs=1, 
+                            type=str, 
+                            default=defaultOutputFile(), 
+                            required=False,
+                            help="CSV file to write the data to. Defaults to the list name as it appears at the end of the URL with '.csv' at the end, in the present directory."
+                            )
+    
+    return vars(arg_parser.parse_args())
+
+
+def main():
+    cli_args = parse_CLI_args()
+
+    get_list_with_attrs(cli_args['list_url'][0],    # sends first argument as a list
+                        cli_args['attributes'],
+                        cli_args['output_file'][0])
+
+    print("\n\nRetrival complete!\n")
+
+
+
+if (__name__ == "__main__"):
+    main()
