5afbf819788156d80458b2c4c554123c3b9d199e
lmr97 <lmreid1997@gmail.com>
2025-03-30 13:36:35 -0600

    added to help build HTML

--- attr-form.py
+++ attr-form.py
@@ -1,9 +1,15 @@
+# This script transforms the list of valid attributes
+# into HTML, formatted like I need for the webpage.
+
 VALID_ATTRS  = []
 with open("./valid-lb-attrs.txt", "r") as attr_file:
     VALID_ATTRS = attr_file.readlines()
 VALID_ATTRS = [a.replace("\n", "") for a in VALID_ATTRS]
 
-html_list = ["\n  </label>\n  <label>\n    <input type=\"checkbox\" id=\""+ a +"\"/>"+a for a in VALID_ATTRS]
+html_list = [
+    "\n  </label>\n  <label>\n    <input type=\"checkbox\" id=\""+ a +"\"/>"+a.replace("-", " ") 
+    for a in VALID_ATTRS
+    ]
 html_list = "".join(html_list)
 html_list = html_list + "\n  </label>"
 
