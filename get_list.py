15c00b5ca9e1cd6a6cfe8cceea222cadcf9708af
lmr97 <lmreid1997@gmail.com>
2025-03-30 13:42:59 -0600

    fouund a way to get list size w/o extra GET, documentation

--- get_list.py
+++ get_list.py
@@ -1,8 +1,8 @@
 # The server translates HTTP GET request query into JSON bytes,
-# and sends them here. since this program is not directly
+# and sends them here. since this program is directly
 # # accessible to the network, it only uses the data format
 # I will give it from the server, and send back text as a
-# # CSV stream of text, with each line delimited by LF (not CRLF).
+# CSV stream of text.
 # Error handling and bundling the data as an HTTP response
 # is also done at the server.
 #
@@ -13,17 +13,24 @@
 #            HTTP      v          |
 #   Client  <---->   Server  ->  JSON
 # 
-# simple as.
+# This script sends the CSV line by line as a byte stream,
+# first sending 2 bytes to denote the total list size, then 
+# for every list row, sending 2 bytes to indicate the row size 
+# (in bytes), and then sending the row data.
+#
+# See custom_backend::lb_app_io.rs for how this data is transmitted
+# to the client.
 
 import json
 import socket
+import re
 from pycurl import Curl
 from selectolax.parser import HTMLParser
 from LetterboxdFilm import LetterboxdFilm, TABBED_ATTRS
-    
-# TODO: correct cast/casting/casting-director
-# [x] here
-# [] README
+
+# parameters for streaming data to server
+ENDIANNESS = 'big'
+SIZE_BYTES = 2
 
 # Superset of TABBED_ATTRS list in letterboxdFilm.py
 VALID_ATTRS = []
@@ -32,133 +39,210 @@ with open("./valid-lb-attrs.txt", "r") as attr_file:
 
 VALID_ATTRS = [a.replace("\n", "") for a in VALID_ATTRS]
 
-def get_film_num_est():
-    return
+
+# get number of films: 
+def send_list_len(html_dom: HTMLParser, conn: socket.socket):
+    """
+    Sends precisely 2 bytes that give the number of films in the list.
+
+    It works by using a description <meta> element in the HTML header 
+    that follows the form "A list of <number> films compiled on
+    Letterboxd, including <film>..." etc. (The user-made description of the 
+    list comes after this standard-issue desc., after "About this list:").
+    """
+    
+    print(" -- Sending total est. len --")
+
+    # the first element is the one we want
+    descr_el  = html_dom.css("meta[name='description']")[0]
+    list_desc = descr_el.attributes['content']
+
+    # get first number in the descr. str (which is list length), 
+    # using a regex search 
+    number_re = re.compile("[0-9]+")
+    len_match = re.search(number_re, list_desc)         # gets first match
+    list_len  = int(len_match[0])                       # cast to int for transmission
+
+    # send it off!
+    int_as_bytes = list_len.to_bytes(SIZE_BYTES, ENDIANNESS)
+    conn.sendall(int_as_bytes)
 
 
-def get_list_with_attrs(query: dict) -> list:
+def get_csv_row(film: LetterboxdFilm, attrs: list, list_is_ranked: bool, list_rank: int):
+    """
+    Assemble a CSV-formatted row of the data from the 
+    requested attributes, and return it as a `str`.
+    """
+    title = "\"" + film.title + "\""            # rudimentary sanitizing
+    file_row = title+","+film.year
+
+    for attr in attrs:
+
+        found_attr = "(not found)"              # default
+        if (attr in TABBED_ATTRS):
+            if (attr == "country"):
+                found_attr = film.get_countries()   # has extra logic needed
+            else:
+                found_attr = film.get_tabbed_attribute(attr)
+        else:
+            match attr:
+                case "avg-rating":  found_attr = film.get_avg_rating()
+                case "cast-list":
+                    found_attr = film.get_casting()
+                    found_attr = [
+                        f"{key}: {value}" for (key, value) in found_attr.items()
+                    ]
+                    found_attr = "; ".join(found_attr)
+                    file_row  += "," + found_attr
+                case "likes":       found_attr = film.get_likes()
+                case "watches":     found_attr = film.get_watches()
+
+        # convert list or dict to CSV-friendly format
+        if (isinstance(found_attr, list)):
+            found_attr = "; ".join(found_attr)  # separate list elements by ";", not "," 
+            file_row  += "," + found_attr
+        else:
+            file_row  += "," + str(found_attr)
+
+    if (list_is_ranked):
+        file_row = str(list_rank) + "," + file_row
+        list_rank += 1
+    
+    return file_row
+
+
+def send_line(conn: socket.socket, line: str):
+    """
+    First sends exactly 2 bytes, containing the number of chars to be written
+    out to the stream (so the server knows how many to read), then 
+    sends that data as bytes.
+    
+    This size-then-data protocol is implemented because Rust's 
+    std::io::read_to_string(), my first choice, reads until EOF is received,
+    and Python doesn't provide a good way to send an EOF signal without 
+    closing a connection. So, Rust's std::io::read_exact() had to be used,
+    for which a set size of buffer must be specified.
+
+    This function is also used to send error messages, using the same protocol.
+    """
+    print(" -- Sending line --")
+    byte_row = bytes(line, 'utf-8')
+
+    # send row length
+    # needs to be byte length of row, to account for multi-byte characters
+    row_len  = len(byte_row)   
+    byte_len = row_len.to_bytes(SIZE_BYTES, ENDIANNESS)
+    print(row_len)
+    print(byte_len)
+    conn.sendall(byte_len)
+
+    # send row itself
+    
+    print(byte_row)
+    conn.sendall(byte_row)
+
+
+def get_list_with_attrs(query: dict, conn: socket.socket) -> None:
     """Gets the requested attributed from the films on a Letterboxd list."""
-    list_file = []
-    unretrieved_attrs = []
+    
     attrs = query['attrs']   # for ease of access
 
+    # config PyCurl and set up base URL
     curl = Curl()
     curl.setopt(curl.HTTPHEADER, ["User-Agent: Application", "Connection: Keep-Alive"])
     lb_list_url = f"https://letterboxd.com/{query['author_user']}/list/{query['list_name']}/"
     
-    # get number of pages
+    # get list's HTML (page 1)
     curl.setopt(curl.URL, lb_list_url)
-    listpage       = curl.perform_rs()
+    listpage       = curl.perform_rs()                
+
+    # check URL validity
+    if (curl.getinfo(curl.HTTP_CODE) != 200):
+        err_msg = f"Error in fetching webpage. Response status: {curl.getinfo(curl.HTTP_CODE)}"
+        send_line(conn, err_msg)
+        raise Exception(err_msg)
+
+    # get number of pages
     tree           = HTMLParser(listpage)
     page_num_nodes = tree.css("li.paginate-page")     # finds all page number nodes
     num_pages      = max(1, len(page_num_nodes))      # guarantee num_pages is at least 1
+
+    send_list_len(tree, conn)                         # send number of films to stream handler on server
+
+    # see if list is ranked
     list_is_ranked = bool(tree.css("p.list-number"))  # casts empty list of nodes to False
     list_rank      = 1                                # defining if needed
 
-    for current_page in range(1, num_pages+1):        # start at 1, including num_pages in iteration
-        # use HTML from the GET that helps initialize page_num,
-        # if on first iteration
+    # compose header
+    header = "Title,Year"
+    for attr in attrs:
+        header += "," + attr.capitalize()
+
+    if (list_is_ranked):
+        header = "Rank," + header
+
+    send_line(conn, header)                           # send out file header
+
+    # Scrap list, by page.
+    # Start at 1, including num_pages value in iteration
+    for current_page in range(1, num_pages+1): 
+
+        # only get new HTML if after first iteration 
+        # (it's already been gotten) during initialization
         if (current_page > 1):
             curl.setopt(curl.URL, lb_list_url+"page/"+str(current_page)+"/")
             listpage = curl.perform_rs()
             tree = HTMLParser(listpage)
 
+        # get URLs of all films on the page
         # a list comprehension, if you can believe it
         film_urls = [
             "https://letterboxd.com" + el.attrs['data-target-link']
             for el in tree.css("div[data-target-link^='/film/']")
         ]
 
+        # get individual film info, and transmit the film's info line
         for url in film_urls:
-            film = LetterboxdFilm(url, curl)            # handled by main
+            
+            film    = LetterboxdFilm(url, curl)
+            csv_row = get_csv_row(film, attrs, list_is_ranked, list_rank)
 
-            title = "\"" + film.title + "\""            # rudimentary sanitizing
-            file_row = title+","+film.year
-
-            for attr in attrs:
-
-                found_attr = "(not found)"              # default
-                if (attr in TABBED_ATTRS):
-                    found_attr = film.get_tabbed_attribute(attr)
-                else:
-                    match attr:
-                        case "avg-rating":  found_attr = film.get_avg_rating()
-                        case "cast-list":
-                            found_attr = film.get_casting()
-                            found_attr = [
-                                f"{key}: {value}" for (key, value) in found_attr.items()
-                            ]
-                            found_attr = "; ".join(found_attr)
-                            file_row  += "," + found_attr
-                        case "likes":       found_attr = film.get_likes()
-                        case "watches":     found_attr = film.get_watches()
-
-                # convert list or dict to CSV-friendly format
-                if (isinstance(found_attr, list)):
-                    found_attr = "; ".join(found_attr)  # separate list elements by ";", not "," 
-                    file_row  += "," + found_attr
-                else:
-                    file_row  += "," + str(found_attr)
-
-            if (list_is_ranked):
-                file_row = str(list_rank) + "," + file_row
-                list_rank += 1
+            send_line(conn, csv_row)
 
             print(f"Info for {film.title} ({film.year}) fetched.")
-            list_file.append(file_row)
+
+            if (list_is_ranked):
+                list_rank += 1
 
         if (current_page == num_pages):
             break
 
         current_page += 1
+    
+    # signal completion to server
+    send_line(conn, "done!")
 
-
-    # finalize header
-    header = "Title,Year"
-    for attr in attrs:
-        if (attr not in unretrieved_attrs):
-            header += "," + attr.capitalize()
-
-    if (list_is_ranked):
-        header = "Rank," + header
-
-    list_file.insert(0, header)
-    return list_file
-
-
-def send_csv(conn: socket.socket, list_data: list):
-    """
-    This function sends only the CSV content, not HTTP response.
-    That is handled by the server.
-    """
-    csv       = "\n".join(list_data) + ""
-    csv_bytes = bytes(csv, "utf-8")
-    conn.sendall(csv_bytes)
 
 def main():
     """See notes at top of file."""
     listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     listener.bind(('0.0.0.0', 3575))
     listener.listen(1)
-    bad_req = bytes("400 BAD REQUEST", "utf-8")
     print(f"Listening on {listener.getsockname()}")
 
     while (True):
+        (conn, _) = listener.accept()
         try:
-            (conn, _) = listener.accept()
             req = conn.recv(2048).decode("utf-8")
             query = json.loads(req)
-            print(query)
 
-            list_csv = get_list_with_attrs(query)
-            send_csv(conn, list_csv)
+            get_list_with_attrs(query, conn)
         except Exception as e:
             print(e)
-            # send only the string "400 BAD REQUEST" and move on
-            # to the next connection.
-            # This is if somehow the client-side JavaScript 
-            # (../static/scripts/lb-app.js) doesn't catch the invalid URL
-            conn.sendall(bad_req) 
+            # send the exception as a string, with
+            # "-- 400 BAD REQUEST --" prepended, and then 
+            # move on to the next connection.
+            send_line(conn, f"-- 400 BAD REQUEST -- {repr(e)}")
             continue
         finally:
             conn.close()    # sends EOF, so that Rust server can read data sent
