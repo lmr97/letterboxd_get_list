cf9fb1cf2ae823a6b5e9dd560daa554923faf568
lmr97 <lmreid1997@gmail.com>
2025-05-14 21:52:24 -0600

    module essentials

--- letterboxd_list/src/letterboxd_list/__init__.py
+++ letterboxd_list/src/letterboxd_list/__init__.py
@@ -1,1 +1,5 @@
-# Python, this is a module.
+"""
+To allow direct importing of the containers without typing
+"letterboxd_list.containers" (boo, hiss)
+"""
+from letterboxd_list.containers import LetterboxdList, LetterboxdFilm, TABBED_ATTRS
