4e71ca735b64572bbbf902bbf40bc6b41b275d7d
lmr97 <lmreid1997@gmail.com>
2025-05-21 22:16:48 -0600

    making module root and valid_attrs module-level globals

--- letterboxd_list/src/letterboxd_list/__init__.py
+++ letterboxd_list/src/letterboxd_list/__init__.py
@@ -1,5 +1,17 @@
 """
-To allow direct importing of the containers without typing
-"letterboxd_list.containers" (boo, hiss)
+A package to collect information about films on letterboxd.com,
+through the lists made on the website. (Note: does not use the
+Letterboxd API, but web scraping techniques, so performance may
+be slow.)
 """
-from letterboxd_list.containers import LetterboxdList, LetterboxdFilm
+from os.path import dirname, realpath
+
+# make VALID_ATTRS available in the while module
+
+# use the module root for absolute path
+MODULE_ROOT = dirname(realpath(__file__))
+VALID_ATTRS = []
+with open(MODULE_ROOT+"/valid-lb-attrs.txt", "r", encoding="utf-8") as attr_file:
+    VALID_ATTRS = attr_file.readlines()
+
+VALID_ATTRS = [a.replace("\n", "") for a in VALID_ATTRS]
