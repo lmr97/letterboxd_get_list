ac2ea4736370ab6ac49faa8a1bccd3b65f193b3b
lmr97 <lmreid1997@gmail.com>
2025-06-09 19:43:32 -0600

    updating test constants

--- letterboxd_list/tests/test_lb_film.py
+++ letterboxd_list/tests/test_lb_film.py
@@ -137,7 +137,7 @@ def test_csv_attrs():
     # cover the stat branches in the function, with margins of error
     # test values retrieved 26 May 2025, about 8:30pm (MDT).
     assert int(TEST_FILM.get_attrs_csv(["watches"])) - 407504 < 10000
-    assert int(TEST_FILM.get_attrs_csv(["likes"]))   - 160815 < 1000
+    assert int(TEST_FILM.get_attrs_csv(["likes"]))   - 162276 < 1000
 
     # since we're checking CSV formatting, we need the raw text data,
     # but from a version without the stats (rating, likes, and watches)
